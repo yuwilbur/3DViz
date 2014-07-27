fb3632b26df58d375059534e72f47118058a6bd8
Matt Tytel <mtytel@leapmotion.com>
2014-07-26 23:03:37 -0700

    Added particles

---
+++ 3DViz/src/ParticleSystem.cpp
@@ -0,0 +1,9 @@
+//
+//  ParticleSystem.cpp
+//  3dViz
+//
+//  Created by Matthew Tytel on 7/26/14.
+//
+//
+
+#include "ParticleSystem.h"
---
+++ 3DViz/src/ParticleSystem.h
@@ -0,0 +1,14 @@
+//
+//  ParticleSystem.h
+//  3dViz
+//
+//  Created by Matthew Tytel on 7/26/14.
+//
+//
+
+#ifndef ___dViz__ParticleSystem__
+#define ___dViz__ParticleSystem__
+
+#include <iostream>
+
+#endif /* defined(___dViz__ParticleSystem__) */
