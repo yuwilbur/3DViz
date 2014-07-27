1f0de4f4a4efda7978892bae8cae2877f89f072b
ansonl <ansonl@users.noreply.github.com>
2014-07-27 09:37:34 -0700

    test

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ def artist_image(artist):
 
 	h = httplib2.Http()
 
-	request = "http://api.openaura.com/v1/search/artists?q=" + artist + "?q=taylor&api_key=outside-hacks"
+	request = "http://api.openaura.com/v1/search/artists?q=" + artist + "&api_key=outside-hacks"
 
 	response, content = h.request(request, "GET")
 	data = json.loads(content)
@@ -20,21 +20,24 @@ def artist_image(artist):
 		artists.append(artist)
 
 	#request = "http://api.openaura.com/v1/classic/artists/" + artists[0]['oa_artist_id'] + "?id_type=oa%3Aartist_id&api_key=outside-hacks"
-	request = "http://api.openaura.com/v1/stream/artists/" + artists[0]['oa_artist_id'] + "?limit=10&id_type=oa:artist_id&api_key=zlA809tV1FCxCb55n5ei0mSmbtHgvpJe"
+	if len(artists) > 0:
+		request = "http://api.openaura.com/v1/stream/artists/" + artists[0]['oa_artist_id'] + "?limit=10&id_type=oa:artist_id&api_key=zlA809tV1FCxCb55n5ei0mSmbtHgvpJe"
 
-	response, content = h.request(request, "GET")
-	data = json.loads(content)
+		response, content = h.request(request, "GET")
+		data = json.loads(content)
 
-	particles = data['particles']
+		particles = data['particles']
 
-	imageUrls = []
+		imageUrls = []
 
-	for particle in particles:
-		media = particle['media']
-		if len(media) > 0:
-			imageUrls.append(media[len(media) - 1])
+		for particle in particles:
+			media = particle['media']
+			if len(media) > 0:
+				imageUrls.append(media[len(media) - 1])
 
-	return json.dumps(imageUrls)
+		return json.dumps(imageUrls)
+	else:
+		return 'No artists found'
 
 @app.route('/events/<artist>')
 def artist_event(artist):
@@ -49,7 +52,7 @@ def artist_event(artist):
 
 	if len(jamBaseArtists) > 0:
 		request = "http://api.jambase.com/events?artistId="+jamBaseArtists[0]['Id']+"&page=0&api_key=c39y82cv6zuzqatrvadjxdm3"
-		print request
+
 		response, content = h.request(request, "GET")
 		data = json.loads(content)
 
@@ -61,6 +64,29 @@ def artist_event(artist):
 			events.append(event)
 
 		return json.dumps(events)
+	else:
+		return "No artists found on jambase"
+
+@app.route('/recommend/<song>')
+def recommend_song(song):
+
+	h = httplib2.Http()
+
+	request = "http://dev.touchtunes.com/music/search?query=" + song + "&api_key=dm6qzugqhamgy2rzwmw3d9j4"
+	response, content = h.request(request, "GET")
+	data = json.loads(content)
+
+	touchTunesTracks = data['response']['result']['songs']
+
+	if len(touchTunesTracks) > 0:
+		request = "http://dev.touchtunes.com/music/recommendations/" + touchTunesTracks[0]['id'] + "?api_key=dm6qzugqhamgy2rzwmw3d9j4"
+
+		response, content = h.request(request, "GET")
+		data = json.loads(content)
+
+		songResults = data['response']['songs']
+
+		return json.dumps(songResult)
 	else:
 		return "No artists found on jambase"
 
