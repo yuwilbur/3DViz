a2541f712c17e4d3193e07119b72a900e520c0be
ansonl <ansonl@users.noreply.github.com>
2014-07-27 10:18:01 -0700

    api client

--- client.py
+++ client.py
@@ -9,7 +9,9 @@ def artist_image(artist):
 
 	h = httplib2.Http()
 
-	request = "http://api.openaura.com/v1/search/artists?q=" + artist + "?q=taylor&api_key=outside-hacks"
+	artist = artist.replace(' ', '+')
+
+	request = "http://api.openaura.com/v1/search/artists?q=" + str(artist) + "?q=taylor&api_key=outside-hacks"
 
 	response, content = h.request(request, "GET")
 	data = json.loads(content)
@@ -19,50 +21,63 @@ def artist_image(artist):
 	for artist in data:
 		artists.append(artist)
 
-	#request = "http://api.openaura.com/v1/classic/artists/" + artists[0]['oa_artist_id'] + "?id_type=oa%3Aartist_id&api_key=outside-hacks"
-	request = "http://api.openaura.com/v1/stream/artists/" + artists[0]['oa_artist_id'] + "?limit=10&id_type=oa:artist_id&api_key=zlA809tV1FCxCb55n5ei0mSmbtHgvpJe"
+	print artists
 
-	response, content = h.request(request, "GET")
-	data = json.loads(content)
+	if len(artists) > 0:
+		#request = "http://api.openaura.com/v1/classic/artists/" + artists[0]['oa_artist_id'] + "?id_type=oa%3Aartist_id&api_key=outside-hacks"
+		request = "http://api.openaura.com/v1/stream/artists/" + str(artists[0]['oa_artist_id']) + "?limit=10&id_type=oa:artist_id&api_key=zlA809tV1FCxCb55n5ei0mSmbtHgvpJe"
 
-	particles = data['particles']
+		response, content = h.request(request, "GET")
+		data = json.loads(content)
 
-	imageUrls = []
+		particles = data['particles']
 
-	for particle in particles:
-		media = particle['media']
-		if len(media) > 0:
-			imageUrls.append(media[len(media) - 1])
+		imageUrls = []
 
-	return json.dumps(imageUrls)
+		for particle in particles:
+			media = particle['media']
+			if len(media) > 0:
+				imageUrls.append(media[len(media) - 1])
+
+		return json.dumps(imageUrls)
+	else:
+		return 'no artists found on openaura'
 
 @app.route('/events/<artist>')
 def artist_event(artist):
 
+	artist = artist.replace(' ', '+')
+
 	h = httplib2.Http()
 
-	request = "http://api.jambase.com/artists?name=" + artist + "&page=0&api_key=c39y82cv6zuzqatrvadjxdm3"
+	request = "http://api.jambase.com/artists?name=" + str(artist) + "&page=0&api_key=c39y82cv6zuzqatrvadjxdm3"
 	response, content = h.request(request, "GET")
 	data = json.loads(content)
 
+	print request
+	print content
+
 	jamBaseArtists = data['Artists']
 
-	if len(jamBaseArtists) > 0:
-		request = "http://api.jambase.com/events?artistId="+jamBaseArtists[0]['Id']+"&page=0&api_key=c39y82cv6zuzqatrvadjxdm3"
-		print request
-		response, content = h.request(request, "GET")
-		data = json.loads(content)
 
-		print len(jamBaseArtists)
 
-		events = []
+	#if len(jamBaseArtists) > 0:
+	firstId = jamBaseArtists[0]['Id']
 
-		for event in data['Events']:
-			events.append(event)
+	eventReq = 'http://api.jambase.com/events?artistId=' + str(firstId) + "&page=0&api_key=c39y82cv6zuzqatrvadjxdm3"
+	response, content = h.request(eventReq, "GET")
+	data = json.loads(content)
 
-		return json.dumps(events)
-	else:
-		return "No artists found on jambase"
+	print eventReq
+
+	events = []
+
+	for event in data['Events']:
+		events.append(event)
+
+	return json.dumps(events)
+	#else:
+		#return "No artists found on jambase"
 
 if __name__ == "__main__":
 	app.run()
