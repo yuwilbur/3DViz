d717797876f64abedf3804591d24f449142638af
Noura <culurienneldoreth@gmail.com>
2014-07-29 17:24:23 -0700

    mapping artist names to images

--- 3DViz/src/ImageInput.cpp
+++ 3DViz/src/ImageInput.cpp
@@ -16,7 +16,20 @@ void ImageInput::setup(std::string artist_name) {
     str_filename_ = "images/artists/Amon Tobin.jpg";
   } else if (artist_name == "Boards of Canada") {
     str_filename_ = "images/artists/Boards of Canada.jpg";
-  } else {
+  } else if (artist_name == "Bear Hands") {
+    str_filename_ = "images/artists/Bear Hands.jpg";
+  } else if (artist_name == "Big Freedia") {
+    str_filename_ = "images/artists/Big Freedia.jpg";
+  } else if (artist_name == "Chromeo") {
+    str_filename_ = "images/artists/Chromeo.jpg";
+  } else if (artist_name == "Grouplove") {
+    str_filename_ = "images/artists/Grouplove.jpg";
+  } else if (artist_name == "John Butler Trio") {
+    str_filename_ = "images/artists/John Butler Trio.png";
+  } else if (artist_name == "Kacey Musgraves") {
+    str_filename_ = "images/artists/Kacey Musgraves.jpg";
+  }
+  else {
     str_filename_ = "images/bleep_or_die.jpg";
   }
   of_img_.loadImage(str_filename_);
